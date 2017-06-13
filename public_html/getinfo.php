da0ef146c9761774cc6d8e102442b34b72a6e01c
szczypiorofix <szczypiorofix@o2.pl>
2017-06-13 16:29:28 +0200

    Całość już w miarę działa. Trzeba tylko poprawić rysowanie tego co przychodzi z serwera.

--- public_html/getinfo.php
+++ public_html/getinfo.php
@@ -2,26 +2,25 @@
     if (isset($_POST['task'])) {
         
         if ($_POST['task']==='clear') {
-            echo 'CLEAR BLACKBOARD!';
-            $array = [];
-            for ($i = 0; $i < (320 * 200); $i++) {
-                $array[$i] = 0;
-            }
+            $array = '';
             $fp = fopen('file.txt', 'w');
-            fwrite($fp, json_encode($array));
+            fwrite($fp, $array);
             fclose($fp);
-            echo json_encode($array);
+            echo 'CLEAR BLACKBOARD!';
         }
         else if ($_POST['task'] === 'add') {
             $array = $_POST['array'];
-            $fp = fopen('file.txt', 'w');
-            fwrite($fp, json_encode($array));
+            $array .= ',';
+            $fp = fopen('file.txt', 'a');
+            //$json_array = json_encode($array);
+            fwrite($fp, $array);
             fclose($fp);
-            echo 'OK! Array length:'.count($array);
+            //echo 'OK! Array length:'.count($array);
+            print $array;
         }
         else if ($_POST['task'] === 'get') {
-            $array = explode( "\n", file_get_contents('file.txt'));
-            echo json_encode($array);
+            $array = file_get_contents('file.txt');
+            echo $array;
         }
         else {
             echo 'Ooops something went wrong, mister...';
@@ -30,3 +29,4 @@
     else {
         echo 'ERROR!';
     }
+?>
